bf516d3a4584ad216bb0f4276f459fe73f2d7ed0
Lee <cleanbackupmail@gmail.com.com>
2024-06-26 01:40:55 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+//import "./App.css";
+//import Navbar from "./components/Navbar";
+//import Footer from "./components/Footer";
+//import Home from "./pages/Home";
+//import Events from "./pages/Events";
+//import Watch from "./pages/Watch";
+//import About from "./pages/About";
+//import Contact from "./pages/Contact";
+//import { BrowserRouter as Router } from "react-router-dom";
+//import 'bootstrap/dist/css/bootstrap.min.css';
+//import './styles.css'
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
