16448364e8dfb74f7c930d6bddfa256424790a41
LEEX0101 <80426125+LEEX0101@users.noreply.github.com>
2024-06-27 23:17:12 -0400

    Update About.tsx

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -6,15 +6,6 @@ function Menu() {
   return (
     <div className="menu">
     <h1 className="menuTitle"> Roster </h1>
-    <div className="aboutRules">
-      <p>
-      Power Slap matches (each a “Match”) consist of two unarmed 
-      combatants (each a “Participant”).
-      </p>
-      <p>The Promoter determines the 
-      number of rounds per Match, but no Match may exceed ten (10) rounds.
-      Participants must be at least 18 years of age.</p>
-    </div>
       <div className="menuList">
         {menuList.map((menuItem, key) => {
           return (
