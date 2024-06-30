a44a3d690d14680ad5f03bef87f2e52e817acc47
LEEX0101 <80426125+LEEX0101@users.noreply.github.com>
2024-06-30 18:55:22 -0400

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,15 +8,17 @@ function Watch() {
     <div className='watch'>
         <div className="overlay"></div>
         <video src={ BackgroundVideo } autoPlay loop muted />
-        <div className="content" style={{color: 'white'}}>
-            <h1>Power Slap 8</h1>
-            <p>RumbleTV</p>
+        <div className="content" style={{color: 'white', display: 'flex', flexDirection: 'row', marginTop: '100px'}}>
             <a href="https://rumble.com/c/powerslap/livestreams">
               <button>
               WATCH LIVE
               </button>
             </a>
-   
+            <a href="https://www.axs.com/">
+              <button>
+              BUY TICKETS
+              </button>
+            </a>
         </div>
         </div>
   );
