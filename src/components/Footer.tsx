7e6b28d1b45813953a03ace8ed3766e5e11939e9
LEEX0101 <80426125+LEEX0101@users.noreply.github.com>
2024-12-04 13:12:55 -0500

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-900 text-white py-12">
+    <footer className="bg-emerald-500 text-white py-12">
       <div className="container mx-auto px-6">
         {/*<div className="flex justify-center items-center">
           <a href="https://linkedin.com" className="hover:text-gray-300">
@@ -11,7 +11,7 @@ const Footer = () => {
           </a>
         </div>*/}
         
-        <div className="border-t border-gray-800 mt-8 pt-8 text-center text-gray-400">
+        <div className="mt-8 pt-8 text-center text-gray-900">
           <p>&copy; {new Date().getFullYear()} LeeDev. All rights reserved.</p>
         </div>
       </div>
